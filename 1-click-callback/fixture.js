75d8f523a2beb20cfa42f92de85856d79c20c854
Ovidiu Cherecheș <skidding@gmail.com>
2018-04-25 16:08:02 +0700

    Allow console.log in fixtures

--- 1-click-callback/fixture.js
+++ 1-click-callback/fixture.js
@@ -3,6 +3,6 @@ import { CompWithBtn } from './components';
 export default {
   component: CompWithBtn,
   props: {
-    onClick: () => {}
+    onClick: () => console.log('Button clicked!')
   }
 };
