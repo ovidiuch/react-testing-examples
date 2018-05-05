9053712821af5185a6a1d1179a88fb8cc24d6a6d
Ovidiu Cherecheș <skidding@gmail.com>
2018-05-05 16:16:17 +0700

    Reuse border radius

--- ui/components/AboutModal.js
+++ ui/components/AboutModal.js
@@ -53,7 +53,7 @@ const Overlay = styled.div`
 
 const Content = CenterText.extend`
   background: #fff;
-  border-radius: 16px;
+  border-radius: 10px;
   box-shadow: 0 3px 15px 0 rgba(0, 0, 0, 0.2);
   overflow: hidden;
 `;
