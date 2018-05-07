8a7103270f5db034418d05ac0be03ae975bc04a5
Ovidiu Cherecheș <skidding@gmail.com>
2018-05-07 12:44:30 +0700

    Explain Babel config

--- .babelrc.js
+++ .babelrc.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: [['babel-plugin-inline-import-data-uri', inlineImportOpts]],
   env: {
     test: {
+      // Jest runs in Node and needs CommonJS modules. So does SSR, but Next
+      // runs webpack on the server as well nowadays
       presets: [['next/babel', { 'preset-env': { modules: 'commonjs' } }]]
     }
   }
