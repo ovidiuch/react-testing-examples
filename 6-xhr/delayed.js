02c6d388a182c89a15562f869bbae9b96bf2fdce
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-16 15:38:25 +0300

    Type delayed utility

--- 6-xhr/delayed.js
+++ 6-xhr/delayed.js
@@ -2,7 +2,7 @@
 
 import delay from 'delay';
 
-export function delayed(val) {
+export function delayed<T>(val: T): Promise<T> {
   return new Promise(async resolve => {
     await delay(200);
     resolve(val);
