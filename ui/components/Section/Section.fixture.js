0ac3d94238ea8d4db99f2ec7265841f34f6f8058
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-17 15:44:12 +0300

    Add fixture

--- ui/components/Section/Section.fixture.js
+++ ui/components/Section/Section.fixture.js
@@ -4,15 +4,29 @@ import { createFixture } from 'react-cosmos';
 import { Section } from '.';
 import { tests } from '../../import-files';
 
-export default createFixture({
-  name: 'Redux test',
-  component: Section,
-  props: {
-    section: {
-      type: 'test',
-      test: tests[4]
-    },
-    testFilter: 'cosmos',
-    searchText: 'Redux'
-  }
-});
+export default [
+  createFixture({
+    name: 'Local component state',
+    component: Section,
+    props: {
+      section: {
+        type: 'test',
+        test: tests[2]
+      },
+      testFilter: 'enzyme',
+      searchText: ''
+    }
+  }),
+  createFixture({
+    name: 'Redux test',
+    component: Section,
+    props: {
+      section: {
+        type: 'test',
+        test: tests[4]
+      },
+      testFilter: 'cosmos',
+      searchText: 'Redux'
+    }
+  })
+];
