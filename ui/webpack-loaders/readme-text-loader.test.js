3f95d4427416be72b964f80afe344c7962d4e74d
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-18 11:37:45 +0300

    Fix failing test

--- ui/webpack-loaders/readme-text-loader.test.js
+++ ui/webpack-loaders/readme-text-loader.test.js
@@ -12,7 +12,7 @@ it('extracts title', () => {
 it('extracts body', () => {
   expect(json.body).toEqual([
     'The component is connected to React Router. It renders a variable text containing a URL parameter, as well as a Link to another location.',
-    'First we make sure the component renders a URL param from the initial URL. Then we check that the URL param from a new location is rendered upon clicking on the Link element, which proves that the page has successfully routed.',
+    'First we make sure the component renders a param from the initial URL. Then we check that the URL param from a new location is rendered upon clicking on the Link element, which proves that the page has successfully routed.',
     `Alternatively, we could just test the to prop of the Link element. That's also fine. But this test is closer to how a user thinks: Click on a link. Did the linked page open?`,
     `This type of thinking makes tests more resilient against implementation changes, like upgrading the router library to a new API.`
   ]);
