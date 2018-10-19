d4f2dd37d830b321454b3a54f3252829de62610f
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-19 15:42:29 +0300

    Render Cosmos fixtures on top of light background

--- ui/cosmos.proxies.js
+++ ui/cosmos.proxies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { GlobalStyle } from './global-style';
 
 function GlobalStyleProxy({ nextProxy, ...otherProps }) {
@@ -7,9 +8,20 @@ function GlobalStyleProxy({ nextProxy, ...otherProps }) {
   return (
     <>
       <GlobalStyle />
-      <NextProxy {...otherProps} nextProxy={next()} />
+      <LightBg>
+        <NextProxy {...otherProps} nextProxy={next()} />
+      </LightBg>
     </>
   );
 }
+
+const LightBg = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #f5f7f9;
+`;
 
 export default [GlobalStyleProxy];
