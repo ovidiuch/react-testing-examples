cfc4537e40cddb460f4961aebb822a667b5de60f
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-19 14:42:55 +0300

    Reorder tests

--- ui/webpack-loaders/import-tests-loader.test.js
+++ ui/webpack-loaders/import-tests-loader.test.js
@@ -11,6 +11,6 @@ const output = loader.call({ addContextDependency: jest.fn() }, input);
 it('embeds tests', () => {
   expect(output).toMatch('jest-enzyme');
   expect(output).toMatch('jest-rtl');
-  expect(output).toMatch('1-click-callback');
+  expect(output).toMatch('1-render-text');
   expect(output).toMatch('8-localstorage');
 });
