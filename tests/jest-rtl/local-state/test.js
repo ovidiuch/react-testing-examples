46af15ad2d7311b57516e0c419badfe5648033ad
Mitchel <51602723+MitchelSt@users.noreply.github.com>
2020-10-09 19:06:38 +0200

    refactor: replaced waitForElement by findByText (#51)
    waitForElement is deprecated

--- tests/jest-rtl/local-state/test.js
+++ tests/jest-rtl/local-state/test.js
@@ -14,15 +14,15 @@ const renderComponent = ({ count }) =>
 
 it('renders initial count', async () => {
   // Render new instance in every test to prevent leaking state
-  const { getByText } = renderComponent({ count: 5 });
+  const { findByText } = renderComponent({ count: 5 });
 
-  await waitForElement(() => getByText(/clicked 5 times/i));
+  expect(await findByText(/clicked 5 times/i))
 });
 
 it('increments count', async () => {
   // Render new instance in every test to prevent leaking state
-  const { getByText } = renderComponent({ count: 5 });
+  const { findByText } = renderComponent({ count: 5 });
 
   fireEvent.click(getByText('+1'));
-  await waitForElement(() => getByText(/clicked 6 times/i));
+  expect(await findByText(/clicked 6 times/i))
 });
